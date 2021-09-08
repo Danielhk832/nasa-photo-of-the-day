d572b6cfa87597563aaf3660e4443e6765fd31ce
Daniel Kish <daniel@Daniels-Mac-mini.local>
2021-09-09 04:19:16 +0700

    finished the assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import { API_KEY, BASE_URL } from "./constants";
 
 function App() {
+
+  const [pic, getPic] = useState('https://www.elegantthemes.com/blog/wp-content/uploads/2019/10/loading-screen-featured-image.jpg')
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}${API_KEY}`)
+      .then(res => {
+        getPic(res.data.url);
+      })
+  })
+
   return (
     <div className="App">
+      <h1>NASA PIC</h1>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <img src={pic} alt='nasa pic'></img>
       </p>
     </div>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key="
+export const API_KEY = "hAWlnBjLaROvtKMRfeXR8q63AOQLKuuZVnVzogEw"
